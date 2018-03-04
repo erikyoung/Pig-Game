6e4e1390daff859e464348aff24b778841fea98e
erikyoung <erik.young@rmit.edu.vn>
2018-03-04 14:20:33 +0700

    created turn taking

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
  scores = [0,0];
  roundScore = 0;
- activePlayer = 1;
+ activePlayer = 0;
 
  
 
@@ -33,6 +33,25 @@ var	dice = Math.floor(Math.random() * 6) + 1;
 var diceDOM = document.querySelector('.dice'); 
 diceDOM.style.display = 'block';
 diceDOM.src = 'dice-' + dice + '.png';
+
+if (dice !== 1) {
+	roundScore += dice;
+	document.querySelector("#current-" + activePlayer).textContent = roundScore;
+} else {
+	activePlayer === 0 ? activePlayer === 1 : activePlayer = 0;
+	roundScore === 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+
+
+
+
+}
 
 }); 
 
